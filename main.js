7814792914e529c818bf9a74466301c7b706293f
Asan <onlyprogrammingjs@gmail.com>
2025-05-31 19:15:44 +0600

    Create a constructor for books

---
+++ main.js
@@ -0,0 +1,13 @@
+const myLibrary = [];
+
+//Create a constructor for books
+function Book(title, author, pages, read) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+}
+
+function addBookToLibrary() {
+  // take params, create a book then store it in the array
+}
