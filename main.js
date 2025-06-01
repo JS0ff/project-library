286a0371a065921bcfd6ae2b92ee05817e845974
Asan <onlyprogrammingjs@gmail.com>
2025-06-01 15:47:57 +0600

    Display book's properties in the card elements

--- main.js
+++ main.js
@@ -20,7 +20,7 @@ function addBookToLibrary(...args) {
 addBookToLibrary("The Hobbit", "Tolkien", "192", "not read yet")
 addBookToLibrary("The Hobbit", "Tolkien", "192", "not read yet")
 addBookToLibrary("The Hobbit", "Tolkien", "192", "not read yet")
-addBookToLibrary("The Hobbit", "Tolkien", "192", "not read yet")
+addBookToLibrary("The Hobbit", "Tolkien.j", "132", "not read yet")
 
 
 // Create a function that loops through the array and displays each book on the page
@@ -28,9 +28,37 @@ const mainContainer = document.querySelector("main")
 
 function displayBooks(){
     for (const element of myLibrary) {
+        // Create book card element
         let bookCard = document.createElement("div")
         bookCard.classList.add("book-card");
-        bookCard.textContent = element.title
+
+        // Book Title
+        let bookTitle = document.createElement("p");
+        bookTitle.classList.add("title")
+        bookTitle.textContent = `Title: ${element.title}`
+
+        // Book Author
+        let bookAuthor = document.createElement('p')
+        bookAuthor.classList.add("author")
+        bookAuthor.textContent = `Author: ${element.author}`
+
+        // Book Pages
+        let bookPages = document.createElement('p');
+        bookPages.classList.add("pages");
+        bookPages.textContent = `Author: ${element.pages}`
+
+        // Book Progress
+        let bookProgress = document.createElement("p");
+        bookProgress.classList.add("progress")
+        bookProgress.textContent = `Progress: ${element.read}`
+        
+        // Append all children to the bookCard element
+        bookCard.appendChild(bookTitle)
+        bookCard.appendChild(bookAuthor)
+        bookCard.appendChild(bookPages)
+        bookCard.appendChild(bookProgress)
+        
+        // Save to the main container
         mainContainer.appendChild(bookCard)
     }
 }
